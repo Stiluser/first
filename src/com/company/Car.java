555fd3be42a3b478fb0ea584dbbc1dad7be397cb
Stiluser <stiluser1@gmail.com>
2020-12-07 16:11:52 +0300

    First commit wtf

--- src/com/company/Car.java
+++ src/com/company/Car.java
@@ -1,4 +1,5 @@
 package com.company;
+
 class Car {
     private int wheight;
     private String model;
@@ -6,49 +7,54 @@ class Car {
     boolean eng_on;
 
     Car() {
-        wheight=0;
-        model="0";
-        fuel_perc=0;
+        wheight = 0;
+        model = "0";
+        fuel_perc = 0;
         eng_on = false;
     }
-    Car (String a){
+
+    Car(String a) {
         model = a;
         wheight = 0;
         fuel_perc = 0;
         eng_on = false;
     }
-    Car(String model,int wheight,int fuel_perc){
+
+    Car(String model, int wheight, int fuel_perc) {
         this.model = model;
         this.wheight = wheight;
         this.fuel_perc = fuel_perc;
         eng_on = false;
     }
 
-    public void start(){
+    public void start() {
         if (eng_on)
-            System.out.println("Машина "+ model +" уже работает");
+            System.out.println("Машина " + model + " уже работает");
         else {
             eng_on = true;
             System.out.println("Машина " + model + " запустилась");
         }
     }
-    public void params(){
+
+    public void params() {
         System.out.println(model + wheight + fuel_perc);
     }
-    public void refill(){
+
+    public void refill() {
         fuel_perc = 100;
     }
-    public void go(){
-        if (fuel_perc>0 && eng_on ) {
+
+    public void go() {
+        if (fuel_perc > 0 && eng_on) {
             fuel_perc--;
             System.out.println("Машина " + model + "едет");
-        }
-        else if (!eng_on)
-            System.out.println("Машину "+ model +"нужно запустить");
-        if (fuel_perc==0)
-            System.out.println("Машина "+ model +"не поедет без топлива");
+        } else if (!eng_on)
+            System.out.println("Машину " + model + "нужно запустить");
+        if (fuel_perc == 0)
+            System.out.println("Машина " + model + "не поедет без топлива");
     }
-    public String getModel(){
+
+    public String getModel() {
         return model;
     }
 }
